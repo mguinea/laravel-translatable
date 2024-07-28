77e97cd5002cc1817d439e8f812b3993e6b773d1
Marc Guinea <develop.marcguinea@gmail.com>
2024-07-28 14:56:50 +0200

    Update Translation.php

--- src/Models/Translation.php
+++ src/Models/Translation.php
@@ -6,9 +6,6 @@ use Illuminate\Database\Eloquent\Model;
 
 class Translation extends Model
 {
-    protected $casts = [
-        'content' => 'array',
-    ];
     protected $fillable = ['content', 'field', 'locale'];
     protected $guarded = ['id'];
     public $timestamps = false;
